f8daafcf817d69aacd2d289f9a2302abe02370f1
Colin <colin.diesh@gmail.com>
2018-07-25 17:14:26 -0400

    Run after initView

--- js/main.js
+++ js/main.js
@@ -11,27 +11,27 @@ return declare( JBrowsePlugin,
     constructor: function( args ) {
         var browser = args.browser;
 
-        // do anything you need to initialize your plugin here
         console.log( "DynamicTrack plugin starting" );
-        var trackConfig = {
-            "label" : "fromfaye",
-            "features" : [{
-                "seq_id" : "ctga",
-                "name" : "Notified",
-                "polarity" : "hijinxed",
-                "description" : "Test feature added via Faye",
-                "end" : 440,
-                "start" : 330
-            }],
-            "storeClass" : "JBrowse/Store/SeqFeature/FromConfig",
-            "type" : "JBrowse/View/Track/CanvasFeatures"
-        };
-        var store = dojo.clone(trackConfig);
-        var track = dojo.clone(trackConfig);
-        store.browser = browser;
-        store.type = store.storeClass;
-        track.store = browser.addStoreConfig(undefined, store);
-        browser.publish ('/jbrowse/v1/v/tracks/new', [track]);
+        browser.afterMilestone('initView', () => {
+            // can be standard track config with a different store class or track type and can add urlTemplate
+            var trackConfig = {
+                "label" : "Dynamic track",
+                "features" : [{
+                    "seq_id" : "ctgA",
+                    "name" : "Example",
+                    "start" : 330,
+                    "end" : 440
+                }],
+                "storeClass" : "JBrowse/Store/SeqFeature/FromConfig",
+                "type" : "JBrowse/View/Track/CanvasFeatures"
+            };
+            var store = dojo.clone(trackConfig);
+            var track = dojo.clone(trackConfig);
+            store.browser = browser;
+            store.type = store.storeClass;
+            track.store = browser.addStoreConfig(undefined, store);
+            browser.publish ('/jbrowse/v1/v/tracks/new', [track]);
+        })
     }
 });
 });
