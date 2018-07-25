2f8e6e748eb77b8204f4446c1ff13f3de03f990b
Colin <colin.diesh@gmail.com>
2018-07-25 17:58:58 -0400

    Use menu item

---
+++ README.md
@@ -0,0 +1,14 @@
+# dynamictrack
+
+A JBrowse plugin that just demonstrates dynamically creating a track
+
+
+## Intallation
+
+Clone the repository to the jbrowse plugins subdirectory and name it DynamicTrack
+
+    git clone https://github.com/cmdcolin/dynamictrack DynamicTrack
+
+Then add the plugin to your configuration, e.g. "plugins": ["DynamicTrack"]
+
+See http://gmod.org/wiki/JBrowse_FAQ#How_do_I_install_a_plugin for details
--- js/main.js
+++ js/main.js
@@ -1,10 +1,18 @@
 define([
            'dojo/_base/declare',
-           'JBrowse/Plugin'
+           'JBrowse/Plugin',
+           'dijit/ConfirmDialog',
+           'dijit/MenuItem',
+           'dijit/registry',
+           'dojo/dom-construct'
        ],
        function(
            declare,
-           JBrowsePlugin
+           JBrowsePlugin,
+           Popup,
+           MenuItem,
+           registry,
+           domConstruct
        ) {
 return declare( JBrowsePlugin,
 {
@@ -13,25 +21,47 @@ return declare( JBrowsePlugin,
 
         console.log( "DynamicTrack plugin starting" );
         browser.afterMilestone('initView', () => {
-            // can be standard track config with a different store class or track type and can add urlTemplate
-            var trackConfig = {
-                "label" : "Dynamic track",
-                "features" : [{
-                    "seq_id" : "ctgA",
-                    "name" : "Example",
-                    "start" : 330,
-                    "end" : 440
-                }],
-                "storeClass" : "JBrowse/Store/SeqFeature/FromConfig",
-                "type" : "JBrowse/View/Track/CanvasFeatures"
-            };
-            var store = dojo.clone(trackConfig);
-            var track = dojo.clone(trackConfig);
-            store.browser = browser;
-            store.type = store.storeClass;
-            track.store = browser.addStoreConfig(undefined, store);
-            browser.publish ('/jbrowse/v1/v/tracks/new', [track]);
-        })
+            browser.addGlobalMenuItem('tools',
+                new MenuItem({
+                    id: 'menubar_popup',
+                    label: 'Dynamic track',
+                    onClick: () => {
+                        new Popup({
+                            browser: browser,
+                            content: 'Click OK to initialize a new track',
+                            title: 'Dynamic track',
+                            onExecute: () => {
+                                // can be standard track config with a different store class or track type and can add urlTemplate
+                                var trackConfig = {
+                                    "label" : "Dynamic track",
+                                    "features" : [{
+                                        "seq_id" : "ctgA",
+                                        "name" : "Example",
+                                        "start" : 330,
+                                        "end" : 440
+                                    }],
+                                    "storeClass" : "JBrowse/Store/SeqFeature/FromConfig",
+                                    "type" : "JBrowse/View/Track/CanvasFeatures"
+                                };
+                                var store = dojo.clone(trackConfig);
+                                var track = dojo.clone(trackConfig);
+                                store.browser = browser;
+                                store.type = store.storeClass;
+                                track.store = browser.addStoreConfig(undefined, store);
+                                browser.publish ('/jbrowse/v1/v/tracks/new', [track]);
+                                alert('Dynamic track created! Go to ctgA:300-400 to see feature');
+                            }
+                        }).show();
+                    }
+                })
+            );
+            setTimeout(() => {
+                browser.renderGlobalMenu('tools', {text: 'Tools'}, browser.menuBar);
+                var helpMenu = registry.byId('dropdownbutton_help');
+                var toolsMenu = registry.byId('dropdownbutton_tools');
+                domConstruct.place(toolsMenu.domNode, helpMenu.domNode, 'before');
+            }, 200)
+        });
     }
 });
 });
